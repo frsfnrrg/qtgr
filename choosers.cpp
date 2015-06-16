edc60e4e0a973f6ad660408e6dfe82d713942878
Frsf Nrrg <frsfnrrg@gmail.com>
2015-06-16 08:08:19 -0400

    configure project autoformat

--- choosers.cpp
+++ choosers.cpp
@@ -40,15 +40,16 @@ void Slider::setValue(int value, bool loud) {
 }
 
 void LongTextEdit::setText(const QString &text, bool loud) {
-    if (!loud) this->blockSignals(true);
+    if (!loud)
+        this->blockSignals(true);
 
     clear();
     moveCursor(QTextCursor::End);
     insertPlainText(text);
 
-    if (!loud) this->blockSignals(false);
+    if (!loud)
+        this->blockSignals(false);
 }
-
 
 // SETS
 
@@ -58,14 +59,12 @@ void LongTextEdit::setText(const QString &text, bool loud) {
  on the combobox. This may lead to creating a model
  for the combobox, but that is fine...
  */
-SetComboBox::SetComboBox(bool a) :
-    QComboBox()
-{
+SetComboBox::SetComboBox(bool a) : QComboBox() {
     add = a;
     if (add) {
         this->addItem("-- All live sets --");
     }
-    for (int i=0;i<MAXPLOT;i++) {
+    for (int i = 0; i < MAXPLOT; i++) {
         this->addItem("");
     }
     updateSets();
@@ -75,11 +74,12 @@ SetComboBox::SetComboBox(bool a) :
 }
 
 void SetComboBox::updateSets() {
-    for (int i=0;i<MAXPLOT;i++) {
+    for (int i = 0; i < MAXPLOT; i++) {
         if (g[cg].p[i].active == OFF && g[cg].p[i].deact == 0) {
-            this->setItemText(i+add, QString("~: ") + g[cg].l.str[i].s);
+            this->setItemText(i + add, QString("~: ") + g[cg].l.str[i].s);
         } else {
-            this->setItemText(i+add, QString::number(i) + ": " + g[cg].l.str[i].s);
+            this->setItemText(i + add,
+                              QString::number(i) + ": " + g[cg].l.str[i].s);
         }
     }
     emit currentIndexChanged(this->currentIndex());
@@ -90,19 +90,17 @@ void SetComboBox::updateSets() {
 // These groups are fonts all have the same metrics; this makes
 // them interchangable, so scaling issues never occur.
 const QString names[4][3] = {
-    {"Liberation Serif","Liberation Sans","Liberation Mono"},// Liberation
-    {"Times New Roman","Arial","Courier New"},// Monotype
-    {"Tinos","Arimo","Cousine"},// Croscore
-    {"","",""}// default
+    {"Liberation Serif", "Liberation Sans", "Liberation Mono"}, // Liberation
+    {"Times New Roman", "Arial", "Courier New"},                // Monotype
+    {"Tinos", "Arimo", "Cousine"},                              // Croscore
+    {"", "", ""}                                                // default
 };
 
 int FontComboBox::serif = -1;
 int FontComboBox::sans = -1;
 int FontComboBox::mono = -1;
 
-FontComboBox::FontComboBox() :
-    QComboBox()
-{
+FontComboBox::FontComboBox() : QComboBox() {
     this->addItem("Serif");
     this->addItem("Serif Bold");
     this->addItem("Serif Italic");
@@ -126,24 +124,25 @@ void FontComboBox::initializeFonts() {
     QFontDatabase q;
     QString t;
     QStringList l = q.families();
-    for (int i=0;i<l.length();i++) {
-        for (int j=0;j<3;j++) {
+    for (int i = 0; i < l.length(); i++) {
+        for (int j = 0; j < 3; j++) {
             t = l.at(i);
-            if (t == names[j][0] && j < serif) serif = j;
-            if (t == names[j][1] && j < sans) sans = j;
-            if (t == names[j][2] && j < mono) mono = j;
+            if (t == names[j][0] && j < serif)
+                serif = j;
+            if (t == names[j][1] && j < sans)
+                sans = j;
+            if (t == names[j][2] && j < mono)
+                mono = j;
         }
     }
 }
 
-QFont FontComboBox::getCurrentFont() {
-    return getFont(this->currentIndex());
-}
+QFont FontComboBox::getCurrentFont() { return getFont(this->currentIndex()); }
 
 QFont FontComboBox::getFont(int index) {
     QFont font;
     int mode = index % 4;
-    switch(mode) {
+    switch (mode) {
     case 0:
         font.setWeight(QFont::Normal);
         font.setItalic(false);
@@ -182,62 +181,55 @@ QFont FontComboBox::getFont(int index) {
 typedef struct {
     int width;
     int height;
-    unsigned char* bits;
+    unsigned char *bits;
 } pattern;
 
 const int MAX_PATTERN = 16;
 
-const pattern patterns[MAX_PATTERN] = {
-    {pat0_width, pat0_height, pat0_bits},
-    {pat1_width, pat0_height, pat1_bits},
-    {pat2_width, pat0_height, pat2_bits},
-    {pat3_width, pat0_height, pat3_bits},
-    {pat4_width, pat0_height, pat4_bits},
-    {pat5_width, pat0_height, pat5_bits},
-    {pat6_width, pat0_height, pat6_bits},
-    {pat7_width, pat0_height, pat7_bits},
-    {pat8_width, pat0_height, pat8_bits},
-    {pat9_width, pat0_height, pat9_bits},
-    {pat10_width, pat0_height, pat10_bits},
-    {pat11_width, pat0_height, pat11_bits},
-    {pat12_width, pat0_height, pat12_bits},
-    {pat13_width, pat0_height, pat13_bits},
-    {pat14_width, pat0_height, pat14_bits},
-    {pat15_width, pat0_height, pat15_bits}
-};
+const pattern patterns[MAX_PATTERN] = {{pat0_width, pat0_height, pat0_bits},
+                                       {pat1_width, pat0_height, pat1_bits},
+                                       {pat2_width, pat0_height, pat2_bits},
+                                       {pat3_width, pat0_height, pat3_bits},
+                                       {pat4_width, pat0_height, pat4_bits},
+                                       {pat5_width, pat0_height, pat5_bits},
+                                       {pat6_width, pat0_height, pat6_bits},
+                                       {pat7_width, pat0_height, pat7_bits},
+                                       {pat8_width, pat0_height, pat8_bits},
+                                       {pat9_width, pat0_height, pat9_bits},
+                                       {pat10_width, pat0_height, pat10_bits},
+                                       {pat11_width, pat0_height, pat11_bits},
+                                       {pat12_width, pat0_height, pat12_bits},
+                                       {pat13_width, pat0_height, pat13_bits},
+                                       {pat14_width, pat0_height, pat14_bits},
+                                       {pat15_width, pat0_height, pat15_bits}};
 
-QPixmap* PatternComboBox::pxs[MAX_PATTERN] = {NULL, NULL, NULL, NULL,
-                                     NULL, NULL, NULL, NULL,
-                                     NULL, NULL, NULL, NULL,
-                                     NULL, NULL, NULL, NULL};
+QPixmap *PatternComboBox::pxs[MAX_PATTERN] = {
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
-PatternComboBox::PatternComboBox() :
-    QComboBox()
-{
-    for (int i=0;i<MAX_PATTERN;i++) {
-        addItem(QIcon(*pxs[i]),QString::number(i));
+PatternComboBox::PatternComboBox() : QComboBox() {
+    for (int i = 0; i < MAX_PATTERN; i++) {
+        addItem(QIcon(*pxs[i]), QString::number(i));
     }
 }
 
-QPixmap PatternComboBox::getPattern(int index) {
-    return *pxs[index];
-}
+QPixmap PatternComboBox::getPattern(int index) { return *pxs[index]; }
 
 void PatternComboBox::initializePatterns() {
-    for (int i=0;i<MAX_PATTERN;i++) {
+    for (int i = 0; i < MAX_PATTERN; i++) {
         pattern pat = patterns[i];
 
         QImage q(pat.width, pat.height, QImage::Format_Mono);
         for (int j = 0; j < pat.height; j++) {
             for (int k = 0; k < pat.width; k++) {
-                int idx = j*pat.width + k;
+                int idx = j * pat.width + k;
                 int byte = idx >> 3;
                 int shift = idx - (byte << 3);
                 q.setPixel(k, j, (pat.bits[j] & (1 << shift)) == 0);
             }
         }
 
-        QPixmap* x = new QPixmap(QPixmap::fromImage(q));
+        QPixmap *x = new QPixmap(QPixmap::fromImage(q));
         pxs[i] = x;
     }
 }
@@ -246,34 +238,33 @@ void PatternComboBox::initializePatterns() {
 
 typedef struct {
     QColor color;
-    const char* name;
+    const char *name;
 } named_color;
 
-named_color colors[MAXCOLORS] =
-{{QColor(255,255,255), "White"},
- {QColor(0,5,0), "Black"},
- {QColor(255,0,0), "Red"},
- {QColor(0,255,0), "Green"},
+named_color colors[MAXCOLORS] = {{QColor(255, 255, 255), "White"},
+                                 {QColor(0, 5, 0), "Black"},
+                                 {QColor(255, 0, 0), "Red"},
+                                 {QColor(0, 255, 0), "Green"},
 
- {QColor(0,0,255), "Blue"},
- {QColor(255,255,0), "Yellow"},
- {QColor(188,143,143), "Brown"},
- {QColor(220,220,220), "Gray"},
+                                 {QColor(0, 0, 255), "Blue"},
+                                 {QColor(255, 255, 0), "Yellow"},
+                                 {QColor(188, 143, 143), "Brown"},
+                                 {QColor(220, 220, 220), "Gray"},
 
- {QColor(148,0,211), "Violet"},
- {QColor(0,255,255), "Cyan"},
- {QColor(255,0,211), "Magenta"},
- {QColor(255,138,0), "Orange"},
+                                 {QColor(148, 0, 211), "Violet"},
+                                 {QColor(0, 255, 255), "Cyan"},
+                                 {QColor(255, 0, 211), "Magenta"},
+                                 {QColor(255, 138, 0), "Orange"},
 
- {QColor(114,33,188), "Blue Violet"},
- {QColor(103,7,72), "Maroon"},
- {QColor(72,209,204), "Turquoise"},
- {QColor(85,192,52), "Mid Green"}};
+                                 {QColor(114, 33, 188), "Blue Violet"},
+                                 {QColor(103, 7, 72), "Maroon"},
+                                 {QColor(72, 209, 204), "Turquoise"},
+                                 {QColor(85, 192, 52), "Mid Green"}};
 
 ColorComboBox::ColorComboBox() {
     QIcon colorIcon;
     QPixmap colorPix(20, 20);
-    for (int i=0;i<MAXCOLORS;i++) {
+    for (int i = 0; i < MAXCOLORS; i++) {
         named_color n = colors[i];
         colorPix.fill(n.color);
         colorIcon.addPixmap(colorPix);
@@ -281,14 +272,12 @@ ColorComboBox::ColorComboBox() {
     }
 }
 
-QColor ColorComboBox::getColor(int index) {
-    return colors[index].color;
-}
+QColor ColorComboBox::getColor(int index) { return colors[index].color; }
 
 // MISC
 
-DoubleSpinBox* makeTextSizer() {
-    DoubleSpinBox* f = new DoubleSpinBox();
+DoubleSpinBox *makeTextSizer() {
+    DoubleSpinBox *f = new DoubleSpinBox();
     f->setMinimum(0.0);
     f->setMaximum(5.0);
     f->setDecimals(2);
@@ -296,16 +285,16 @@ DoubleSpinBox* makeTextSizer() {
     return f;
 }
 
-QComboBox* makeWidthSelector() {
-    QComboBox* f = new QComboBox();
-    for (int i=0;i<MAX_LINEWIDTH;i++) {
-        f->addItem(QString::number(i+1));
+QComboBox *makeWidthSelector() {
+    QComboBox *f = new QComboBox();
+    for (int i = 0; i < MAX_LINEWIDTH; i++) {
+        f->addItem(QString::number(i + 1));
     }
     return f;
 }
 
-QComboBox* makeLineStyler() {
-    QComboBox* f = new QComboBox();
+QComboBox *makeLineStyler() {
+    QComboBox *f = new QComboBox();
     f->addItem("None");
     f->addItem("Solid");
     f->addItem("Dashed");
@@ -317,31 +306,29 @@ QComboBox* makeLineStyler() {
 
 // Range selectors...
 
-IntegerRangeSelector::IntegerRangeSelector(int min, int max, int step) :
-    QWidget()
-{
+IntegerRangeSelector::IntegerRangeSelector(int min, int max, int step)
+    : QWidget() {
     slider = new Slider();
     slider->setTickPosition(QSlider::TicksAbove);
     slider->setRange(min, max);
     slider->setSingleStep(step);
     slider->setPageStep(step);
     slider->setTickInterval(step);
-    connect(slider, SIGNAL(valueChanged(int)), this, SLOT(sliderValueUpdated(int)));
+    connect(slider, SIGNAL(valueChanged(int)), this,
+            SLOT(sliderValueUpdated(int)));
 
     box = new IntegerSpinBox();
     box->setRange(min, max);
     box->setSingleStep(step);
     connect(box, SIGNAL(valueChanged(int)), this, SLOT(boxValueUpdated(int)));
 
-    QHBoxLayout* layout = new QHBoxLayout();
+    QHBoxLayout *layout = new QHBoxLayout();
     layout->addWidget(box);
     layout->addWidget(slider);
     this->setLayout(layout);
 }
 
-int IntegerRangeSelector::value() {
-    return slider->value();
-}
+int IntegerRangeSelector::value() { return slider->value(); }
 
 void IntegerRangeSelector::setValue(int v) {
     slider->setValue(v);
@@ -368,12 +355,13 @@ void IntegerRangeSelector::setDisabled(bool b) {
     slider->setDisabled(b);
 }
 
-DoubleRangeSelector::DoubleRangeSelector(double min, double max, int prec, double step) :
-    QWidget()
-{
+DoubleRangeSelector::DoubleRangeSelector(double min, double max, int prec,
+                                         double step)
+    : QWidget() {
     int iscale = 1;
-    for (int i=0;i<prec;i++) iscale *= 10;
-    scale = (double) iscale;
+    for (int i = 0; i < prec; i++)
+        iscale *= 10;
+    scale = (double)iscale;
     invscale = 1 / scale;
 
     slider = new Slider();
@@ -382,23 +370,23 @@ DoubleRangeSelector::DoubleRangeSelector(double min, double max, int prec, doubl
     slider->setSingleStep(step * scale);
     slider->setPageStep(step * scale);
     slider->setTickInterval(step * scale);
-    connect(slider, SIGNAL(valueChanged(int)), this, SLOT(sliderValueUpdated(int)));
+    connect(slider, SIGNAL(valueChanged(int)), this,
+            SLOT(sliderValueUpdated(int)));
 
     box = new DoubleSpinBox();
     box->setRange(min, max);
     box->setSingleStep(step);
     box->setDecimals(prec);
-    connect(box, SIGNAL(valueChanged(double)), this, SLOT(boxValueUpdated(double)));
+    connect(box, SIGNAL(valueChanged(double)), this,
+            SLOT(boxValueUpdated(double)));
 
-    QHBoxLayout* layout = new QHBoxLayout();
+    QHBoxLayout *layout = new QHBoxLayout();
     layout->addWidget(box);
     layout->addWidget(slider);
     this->setLayout(layout);
 }
 
-double DoubleRangeSelector::value() {
-    return box->value();
-}
+double DoubleRangeSelector::value() { return box->value(); }
 
 void DoubleRangeSelector::setValue(double v) {
     slider->setValue(v * scale);
@@ -426,12 +414,12 @@ void DoubleRangeSelector::setDisabled(bool b) {
     slider->setDisabled(b);
 }
 
-DoubleSpinBox* getUnitDoubleSpinBox() {
-   DoubleSpinBox* f = new DoubleSpinBox();
-   f->setMaximum(1.0);
-   f->setMinimum(0.0);
-   f->setDecimals(3);
-   f->setSingleStep(0.05);
-   f->setCorrectionMode(QAbstractSpinBox::CorrectToNearestValue);
-   return f;
+DoubleSpinBox *getUnitDoubleSpinBox() {
+    DoubleSpinBox *f = new DoubleSpinBox();
+    f->setMaximum(1.0);
+    f->setMinimum(0.0);
+    f->setDecimals(3);
+    f->setSingleStep(0.05);
+    f->setCorrectionMode(QAbstractSpinBox::CorrectToNearestValue);
+    return f;
 }
